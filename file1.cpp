138359e4146e1df46b5d729722f6395329cb84b3
dashborr <danisarfas7@gmail.com>
2026-03-13 14:35:35 +0700

    Menambahkan fungsi input data array

--- file1.cpp
+++ file1.cpp
@@ -3,3 +3,26 @@ using namespace std;
 
 int arr[20]
 int n;
+
+void input (){
+    while (true){
+        cout << "masukan jumlah data pada array : ";
+        cin >> n;
+
+        if (n <= 20){
+            break;
+        }
+        else{
+            cout << "\nArray Tidak Boleh Lebih Dari20\n";
+        }
+    }
+    cout << endl;
+    cout << "============================================="
+    cout << "==========Masukan Element Array=============="
+    cout << "============================================="
+
+    for (int i = 0; i < n; i++ ){
+        cout << "Data ke-" << (i+1) << ": ";
+        cin >> arr{i};
+    }
+}
